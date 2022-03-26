815065f4d8345b64213eb02ae42c2e656b9e7232
Maria <mgordienkom@gmail.com>
2022-03-26 23:44:25 +0100

    add menu footer styles and images

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="app_main")
+     * @Route("/home", name="home")
      */
     public function index(): Response
     {
