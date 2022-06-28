d50a9d71f9d508916ae6093c55dc30806a497adc
Maria <mgordienkom@gmail.com>
2022-06-28 15:37:17 +0200

    edit regeitration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,36 +4,51 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('name', TextType::class, [
+                'label_format' => 'Имя',
+                'attr' => ['class' => 'uk-input']
             ])
+            ->add('email', EmailType::class, [
+                "attr" => ['class' => 'uk-input'],
+                'label_format' => 'Email',
+                // "placeholder" => "Email",
+            ])
+            
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'Vous devez accepter nos conditions.',
+            //         ]),
+            //     'label_format' => 'Accepter des conditions',
+            //     'attr' => ['class' => 'uk-checkbox']
+            //     ],
+            // ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'uk-input'],
+                'label_format' => 'Пароль',
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Введите пароль',
                     ]),
                     new Length([
                         'min' => 6,
@@ -43,10 +58,14 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('submit', SubmitType::class, [
+                'label_format' => 'Зарегистрироваться',
+                'attr' => ['class' => 'uk-button uk-button-secondary uk-margin-top']
+            ]);
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
